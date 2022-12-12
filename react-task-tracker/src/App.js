8fc684f82f48f37f8649a03aca7e0348b2204fca
Justin <justinrkinsman@gmail.com>
2022-12-12 12:04:40 -0400

    Import header component to app.js

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,10 +1,12 @@
+import Header from "./components/Header";
+
 function App() {
   const name = 'Justin'
   const x = true
 
   return (
-    <div className="App">
-      <h1>Hello From React</h1>
+    <div className="container">
+      <Header />
       <h2>Hello {name}</h2>
       <h2>Hello {x ? 'Yes' : "No"}</h2>
     </div>

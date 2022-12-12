9ae7d33e729fc2fce54942e224193ab8d16c405d
Justin <justinrkinsman@gmail.com>
2022-12-12 14:11:17 -0400

    Create function to add task to tasks list

--- react-task-tracker/src/components/AddTask.js
+++ react-task-tracker/src/components/AddTask.js
@@ -1,12 +1,27 @@
 import { useState } from 'react'
 
-const AddTask = () => {
+const AddTask = ({ onAdd }) => {
     const [text, setText] = useState('')
     const [day, setDay] = useState('')
     const [reminder, setReminder] = useState(false)
 
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if(!text) {
+            alert('Please add a task')
+            return
+        }
+
+        onAdd({ text, day, reminder })
+
+        setText('')
+        setDay('')
+        setReminder(false)
+    }
+
     return (
-        <form className='add-form'>
+        <form className='add-form' onSubmit={onSubmit}>
             <div className='form-control'>
                 <label>Task</label>
                 <input 
@@ -23,10 +38,11 @@ const AddTask = () => {
                     value={day} 
                     onChange={(e) => setDay(e.target.value)}/>
             </div>
-            <div className='form-control from-control-check'>
+            <div className='form-control form-control-check'>
                 <label>Set Reminder</label>
                 <input 
-                    type='checkbox' 
+                    type='checkbox'
+                    checked={reminder}
                     value={reminder} 
                     onChange={(e) => setReminder(e.currentTarget.checked)}/>
             </div>

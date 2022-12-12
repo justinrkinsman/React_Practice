74e38069f6ec11625473681ed8336251bbd7adc9
Justin <justinrkinsman@gmail.com>
2022-12-12 12:03:19 -0400

    Create header component

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,7 +1,12 @@
 function App() {
+  const name = 'Justin'
+  const x = true
+
   return (
     <div className="App">
       <h1>Hello From React</h1>
+      <h2>Hello {name}</h2>
+      <h2>Hello {x ? 'Yes' : "No"}</h2>
     </div>
   );
 }

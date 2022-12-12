83f332a9fa00b6e510cf6d3cb427663cada26915
Justin <justinrkinsman@gmail.com>
2022-12-12 12:25:57 -0400

    Pass onClick function to button as prop

--- react-task-tracker/src/components/Button.js
+++ react-task-tracker/src/components/Button.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types'
 
-const Button = ({ color, text }) => {
-  return (
-    <button style={{backgroundColor: color}} className='btn'>{text}</button>
-  )
+const Button = ({ color, text, onClick }) => {
+    return (
+        <button onClick={onClick} style={{backgroundColor: color}} className='btn'>{text}</button>
+    )
 }
 
 Button.defaultProps = {
@@ -13,6 +13,7 @@ Button.defaultProps = {
 Button.propTypes = {
     text: PropTypes.string,
     color: PropTypes.string,
+    onClick: PropTypes.func,
 }
 
 export default Button
